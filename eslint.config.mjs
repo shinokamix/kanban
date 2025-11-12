4f63c09a3ef3defefc9e402cf7d67ef07cb4e321
Nail <nail.dev@icloud.com>
2025-11-12 20:15:36 +0300

    chore: initial setup (Next 16, ESLint+Prettier, FSD)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,43 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+// eslint.config.mjs
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import featureSliced from '@conarti/eslint-plugin-feature-sliced'
+import importPlugin from 'eslint-plugin-import'
+import prettierConfig from 'eslint-config-prettier'
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+export default defineConfig([
+    // глобальные игноры
+    globalIgnores([
+        '.next/**',
+        'out/**',
+        'dist/**',
+        'build/**',
+        'coverage/**',
+        'node_modules/**',
+        '.storybook-static/**',
+        'cypress/screenshots/**',
+        'cypress/videos/**',
+    ]),
 
-export default eslintConfig;
+    // базовые конфиги Next
+    ...nextVitals,
+    ...nextTs,
+
+    {
+        plugins: {
+            '@conarti/feature-sliced': featureSliced,
+            import: importPlugin,
+        },
+        rules: {
+            // FSD: слои и слайсы
+            '@conarti/feature-sliced/layers-slices': 'error',
+            // FSD: абсолютные/относительные импорты
+            '@conarti/feature-sliced/absolute-relative': 'error',
+            // FSD: импорт только из public API
+            '@conarti/feature-sliced/public-api': 'error',
+        },
+    },
+
+    prettierConfig,
+])
