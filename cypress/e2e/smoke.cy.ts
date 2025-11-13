3dcec20eef713aaaead9b0f8618f2e1854269965
Nail <nail.dev@icloud.com>
2025-11-13 15:28:11 +0300

    "fix: update .gitignore and Cypress config"

--- cypress/e2e/smoke.cy.ts
+++ cypress/e2e/smoke.cy.ts
@@ -1,6 +1,6 @@
 describe('App smoke', () => {
     it('opens home page', () => {
-        cy.visit('/') // работает благодаря baseUrl
-        cy.contains('html') // простая проверка, чтобы увидеть PASS
+        cy.visit('/')
+        cy.get('body').should('be.visible')
     })
 })
