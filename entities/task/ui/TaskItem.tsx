17fef6149090e9faf644a961b3e3b11aa7089fbe
Nail Abdullin <123063979+shinokamix@users.noreply.github.com>
2025-11-16 21:51:01 +0300

    Refactor/task card (#6)
    * chore: shadcn item setup
    
    * refactor: implemented shadcn Item
    
    * refactor: renamed TaskCard -> TaskItem and refactor imports
    
    * refactor: changed size to "sm"
    
    * refactor: removed "use client"

---
+++ entities/task/ui/TaskItem.tsx
@@ -0,0 +1,16 @@
+import type { Task } from '../model/task.types'
+import { Item, ItemContent, ItemTitle } from '@/shared/ui/item'
+
+interface TaskCardProps {
+    task: Task
+}
+
+export default function TaskItem({ task }: TaskCardProps) {
+    return (
+        <Item variant={'outline'} size={'sm'}>
+            <ItemContent>
+                <ItemTitle>{task.title}</ItemTitle>
+            </ItemContent>
+        </Item>
+    )
+}
