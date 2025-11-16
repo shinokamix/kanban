b1b118a3e72762ca75d245026bfd80814d764fb2
Nail <nail.dev@icloud.com>
2025-11-17 00:20:28 +0300

    refactor: implemented create task api

--- features/create-task/model/useCreateTask.ts
+++ features/create-task/model/useCreateTask.ts
@@ -3,24 +3,21 @@
 import { useCallback } from 'react'
 import { useAppDispatch } from '@/providers/StoreProvider'
 import { taskAdded, type TaskStatus } from '@/entities/task'
+import { createTaskApi } from '../api/createTask'
 import type { CreateTaskFormValues } from './schema'
 
 export function useCreateTask(initialStatus: TaskStatus = 'todo') {
     const dispatch = useAppDispatch()
 
     const createTask = useCallback(
-        (values: CreateTaskFormValues) => {
-            // здесь мы на основе данных формы создаём полноценный Task
-            dispatch(
-                taskAdded({
-                    id: crypto.randomUUID(),
-                    title: values.title,
-                    description: values.description ?? '',
-                    status: initialStatus,
-                    createdAt: new Date().toISOString(),
-                    updatedAt: new Date().toISOString(),
-                }),
-            )
+        async (values: CreateTaskFormValues) => {
+            const created = await createTaskApi({
+                title: values.title,
+                description: values.description ?? '',
+                status: initialStatus,
+            })
+
+            dispatch(taskAdded(created))
         },
         [dispatch, initialStatus],
     )
