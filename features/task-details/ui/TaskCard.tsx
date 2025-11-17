db52150625e34d6424adada7d26646fd42b0df06
Nail <nail.dev@icloud.com>
2025-11-18 01:48:42 +0300

    feat: made TaskCard draggable and sortable

--- features/task-details/ui/TaskCard.tsx
+++ features/task-details/ui/TaskCard.tsx
@@ -2,8 +2,13 @@
 
 import { useState } from 'react'
 import type { Task } from '@/entities/task'
-import { TaskItem } from '@/entities/task'
 import { TaskDetailsDialog } from './TaskDetailsDialog'
+import { Grip } from 'lucide-react'
+
+import { Item, ItemActions, ItemContent } from '@/shared/ui/item'
+
+import { useSortable } from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
 
 type TaskCardWithDetailsProps = {
     task: Task
@@ -11,10 +16,25 @@ type TaskCardWithDetailsProps = {
 
 export function TaskCard({ task }: TaskCardWithDetailsProps) {
     const [open, setOpen] = useState(false)
+    const { attributes, listeners, setNodeRef, transform, transition } = useSortable({
+        id: task.id,
+    })
 
     return (
         <>
-            <TaskItem task={task} onClick={() => setOpen(true)} />
+            <Item variant={'outline'} size={'sm'} onClick={() => setOpen(true)}>
+                <ItemContent>
+                    <p>{task.title}</p>
+                </ItemContent>
+                <ItemActions
+                    ref={setNodeRef}
+                    style={{ transform: CSS.Transform.toString(transform), transition }}
+                    {...attributes}
+                    {...listeners}
+                >
+                    <Grip />
+                </ItemActions>
+            </Item>
             <TaskDetailsDialog taskId={task.id} open={open} onOpenChange={setOpen} />
         </>
     )
