3bf7d9a111aa67742aebbe0154e7c73ade7f2e1b
Nail <nail.dev@icloud.com>
2025-11-18 01:16:05 +0300

    refactor: implemented columns

--- widgets/task-board/ui/TaskBoard.tsx
+++ widgets/task-board/ui/TaskBoard.tsx
@@ -3,11 +3,22 @@
 import { useLoadTasks } from '../model/useLoadTasks'
 import { useAppSelector } from '@/providers/StoreProvider'
 import { taskSelectors } from '@/entities/task'
+import Column from './Column'
 
 export function TaskBoard() {
     useLoadTasks()
 
     const tasks = useAppSelector(taskSelectors.selectAll)
 
-    return <section className="grid grid-cols-3 gap-10"></section>
+    const todo = tasks.filter((task) => task.status === 'todo')
+    const inProgress = tasks.filter((task) => task.status === 'in-progress')
+    const done = tasks.filter((task) => task.status === 'done')
+
+    return (
+        <section className="grid grid-cols-3 gap-[2%]">
+            <Column tasks={todo} />
+            <Column tasks={inProgress} />
+            <Column tasks={done} />
+        </section>
+    )
 }
