4baf6a2fa7bcc326dbcdeb5b4c564214456ac8fe
Nail <nail.dev@icloud.com>
2025-11-18 01:25:01 +0300

    refactor: added labels

--- widgets/task-board/ui/TaskBoard.tsx
+++ widgets/task-board/ui/TaskBoard.tsx
@@ -4,6 +4,7 @@ import { useLoadTasks } from '../model/useLoadTasks'
 import { useAppSelector } from '@/providers/StoreProvider'
 import { taskSelectors } from '@/entities/task'
 import Column from './Column'
+import { Label } from '@shared/ui/label'
 
 export function TaskBoard() {
     useLoadTasks()
@@ -16,9 +17,18 @@ export function TaskBoard() {
 
     return (
         <section className="grid grid-cols-3 gap-[2%]">
-            <Column tasks={todo} />
-            <Column tasks={inProgress} />
-            <Column tasks={done} />
+            <section>
+                <Label className="py-4">To-do</Label>
+                <Column tasks={todo} />
+            </section>
+            <section>
+                <Label className="py-4">In progress</Label>
+                <Column tasks={inProgress} />
+            </section>
+            <section>
+                <Label className="py-4">Done</Label>
+                <Column tasks={done} />
+            </section>
         </section>
     )
 }
