b4c61562009aaa2841ff24c1d60c1c50be3cd833
Nail <nail.dev@icloud.com>
2025-11-24 00:01:52 +0300

    refactor: v1

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,34 @@
 import Link from 'next/link'
-import { Github } from 'lucide-react'
+import { GithubIcon } from 'lucide-react'
 import { Button } from '@/shared/ui/button'
 
 export default function Home() {
     return (
-        <main className="flex h-screen justify-center items-center">
-            <div className="flex flex-col gap-[3dvh] max-w-[30%]">
-                <section className="flex flex-col">
-                    <h1 className="font-medium text-[8dvw] text-">Kanban</h1>
-                    <p className="text-[1dvw]">
-                        A web-based Kanban board application for organizing tasks and workflows
-                        using visual columns and draggable cards.
+        <main className="flex min-h-screen items-center justify-center px-8 py-8">
+            <div className="flex w-full max-w-2xl flex-col gap-8 md:max-w-xl">
+                {/* Text */}
+                <section className="flex flex-col gap-4">
+                    <h1 className="font-medium text-4xl leading-tight text-balance md:text-6xl">
+                        Kanban
+                    </h1>
+                    <p className="text-base leading-relaxed text-muted-foreground md:text-lg">
+                        A minimal Kanban board for keeping work organized. Create tasks, move them
+                        between columns and always see what’s in progress and what’s done — clean,
+                        simple and nothing beyond the basics.
                     </p>
                 </section>
 
-                <section className="flex items-center gap-1">
-                    <Link href={'/kanban'}>
-                        <Button>Go to app</Button>
+                {/* Buttons */}
+                <section className="flex gap-3 flex-row">
+                    <Link href="/kanban">
+                        <Button className="items-center">Launch app</Button>
                     </Link>
-                    <Link href={'https://github.com/shinokamix/kanban'}>
-                        <Button variant={'outline'}>
-                            <Github />
+
+                    <Link href="https://github.com/shinokamix/kanban">
+                        <Button variant="outline" className=" gap-2">
+                            <GithubIcon className="h-5 w-5" />
+
+                            <span className="hidden sm:inline">GitHub</span>
                         </Button>
                     </Link>
                 </section>
