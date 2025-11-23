7761414cd5337ff8b6ed3867fff980ca478995c4
Nail <nail.dev@icloud.com>
2025-11-23 23:28:56 +0300

    refactor: v0

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,30 @@
 import Link from 'next/link'
+import { Github } from 'lucide-react'
+import { Button } from '@/shared/ui/button'
 
 export default function Home() {
     return (
         <main className="flex h-screen justify-center items-center">
-            <Link
-                href={'/kanban'}
-                className="py-2 px-5 border rounded-2xl hover:opacity-70 transition-opacity duration-200"
-            >
-                Kanban
-            </Link>
+            <div className="flex flex-col gap-[3dvh] max-w-[30%]">
+                <section className="flex flex-col">
+                    <h1 className="font-medium text-[8dvw] text-">Kanban</h1>
+                    <p className="text-[1dvw]">
+                        A web-based Kanban board application for organizing tasks and workflows
+                        using visual columns and draggable cards.
+                    </p>
+                </section>
+
+                <section className="flex items-center gap-1">
+                    <Link href={'/kanban'}>
+                        <Button>Go to app</Button>
+                    </Link>
+                    <Link href={'https://github.com/shinokamix/kanban'}>
+                        <Button variant={'outline'}>
+                            <Github />
+                        </Button>
+                    </Link>
+                </section>
+            </div>
         </main>
     )
 }
